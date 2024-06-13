53702b10ab28d3c0661faaa0b4b566d4ecd29e24
xmen02052003lx <97832906+xmen02052003lx@users.noreply.github.com>
2024-06-13 15:48:14 +0700

    merged cart feature

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 import { useState } from "react"
 
 import Header from "./components/Layout/Header"
 import Meals from "./components/Meals/Meals"
 import CartProvider from "./store/CartProvider"
 import Cart from "./components/Cart/Cart"
-=======
-import Header from "./components/Layout/Header"
-import Meals from "./components/Meals/Meals"
-import CartProvider from "./store/CartProvider"
-import Cart from "./components/Cart/Cart"
->>>>>>> feat/cart
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false)
@@ -25,7 +18,7 @@ function App() {
 
   return (
     <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
